b56c0f326e4eca010e353213937c48d214cea9d2
Rick Meels <rick.meels@gmail.com>
2020-11-22 23:05:10 +0100

    Added problem4

---
+++ Euler/Problem4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = 0;
+            for (int i = 999; i > 100; i--)
+            {
+                for (int j = 999; j > 100; j--)
+                {
+                    var temp = i * j;
+                    if (temp.ToString() == new string(temp.ToString().Reverse().ToArray()))
+                    {
+                        if (temp > result)
+                        {
+                            result = temp;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
