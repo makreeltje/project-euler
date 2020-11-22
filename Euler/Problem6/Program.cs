338b0c5d7bf0631070af9b755524b8be186b3e95
Rick Meels <rick.meels@gmail.com>
2020-11-22 23:46:19 +0100

    Added problem 6

---
+++ Euler/Problem6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int squareNumber = 0;
+            for (int i = 1; i < 101; i++)
+            {
+                number += i * i;
+                squareNumber += i;
+            }
+
+            var result = (squareNumber * squareNumber) - number;
+            
+            Console.WriteLine(result);
+        }
+    }
+}
