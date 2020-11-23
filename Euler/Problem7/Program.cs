602858c9c9c55dd8a52318c73f0c691fd0a9ea7d
Rick Meels <rick.meels@gmail.com>
2020-11-23 17:35:43 +0100

    Added problem7

---
+++ Euler/Problem7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            int num = 2;
+            while (true)
+            {
+                if (isPrime(num))
+                {
+                    count++;
+                }
+
+                if (count == 10001)
+                {
+                    break;
+                }
+
+                num++;
+            }
+
+            Console.WriteLine(num);
+        }
+
+        static bool isPrime(int num)
+        {
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
