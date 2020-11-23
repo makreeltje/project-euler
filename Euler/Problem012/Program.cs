51b01811e740fc97e058fc49fbe42d163c3a87ff
Rick Meels <rick.meels@gmail.com>
2020-11-23 21:41:51 +0100

    Added problem12

---
+++ Euler/Problem012/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Problem012
+{
+    class Program
+    {
+        private static Stopwatch _stopwatch = new Stopwatch();
+        static void Main(string[] args)
+        {
+            _stopwatch.Start();
+            var num = 0;
+            var i = 1;
+
+            while (numOfDivisors(num) < 500)
+            {
+                num += i;
+                i++;
+            }
+            
+            Console.WriteLine("Result: " + num);
+            _stopwatch.Stop();
+            Console.WriteLine("Time: " + _stopwatch.Elapsed);
+            
+        }
+        
+        static int numOfDivisors(int num)
+        {
+            int nod = 0;
+            int sqrt = (int) Math.Sqrt(num);
+
+            for(int i = 1; i<= sqrt; i++){
+                if(num % i == 0){
+                    nod += 2;
+                }
+            }
+            //Correction if the number is a perfect square
+            if (sqrt * sqrt == num) {
+                nod--;
+            }
+                        
+            return nod;
+        } 
+    }
+}
